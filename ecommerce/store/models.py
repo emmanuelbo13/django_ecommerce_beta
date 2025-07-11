e6fb61daee7d6223f300a8974f21e30f52d5317d
cyber_e <bustemma90@gmail.com>
2025-07-11 01:22:59 -0500

    added Category and Product model
    test models with dummy data and add User model and build authentication functionality.

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, null=True, blank=True)
+    
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+        
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    slug = models.SlugField(max_length=100, unique=True, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    # stock = models.PositiveIntegerField(default=0) Not sure if we need this field yet.
+
+    class Meta:
+        verbose_name_plural = 'Products' # define plural for admin site.
+        ordering = ('-created_at',)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+        
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
+    
+    def __str__(self):
+        return self.name
+
+    # def is_in_stock(self):
+    #     return self.stock > 0
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Product, Category
 
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'store/index.html'
+    model = Product
+    category = Category
+    context_object_name = 'products'
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = Product.objects.all()
+        context['categories'] = Category.objects.all()
+        return context
+
+
