dc0a26825b69912cf65e8d95cf348a8d93d41452
cyber_e <bustemma90@gmail.com>
2025-07-24 00:57:51 -0500

    added order and orderitem models as part of cart and purchase functionality

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
+from django.conf import settings
+from users.models import CustomUser, Address
 
 # Create your models here.
 class Category(models.Model):
@@ -60,3 +62,49 @@ class Product(models.Model):
 
     # def is_in_stock(self):
     #     return self.stock > 0
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+    shipping_address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True, blank=True)
+
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('processing', 'Processing'),
+        ('shipped', 'Shipped'),
+        ('completed', 'Completed'),
+        ('cancelled', 'Cancelled'),
+        ('returned', 'Returned'),
+    )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    payment_transaction_id = models.CharField(max_length=100, blank=True, null=True)
+
+    def get_absolute_url(self):
+        return reverse('store:order_detail', args=[self.id])
+
+    def __str__(self):
+        return f"Order {self.id} - {self.status}"
+    
+    class Meta:
+        verbose_name_plural = 'Orders'
+        verbose_name = 'Order'
+        ordering = ('-created_at',)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.SET_NULL, null=True, blank=True)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name if self.product else 'Deleted Product'}"
+    
+    def get_total_item_price(self):
+        return self.price * self.quantity
+    
+    class Meta:
+        verbose_name_plural = 'Order Items'
+        verbose_name = 'Order Item'
+        ordering = ('-order__created_at',)
